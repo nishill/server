60e8d0b784ebe4e774c1b8a13c187bd3570e4115
Nick Hill <nishill@ucsc.edu>
2016-09-13 15:10:04 -0700

    added input arg to mine_file.py for output file name

--- scripts/mine_file.py
+++ scripts/mine_file.py
@@ -5,7 +5,8 @@ Takes as input a column and filename. The file must include columns (tsv or csv)
 The column name is processed into a json file with the columns as keys in the .json.
 
 run:
-python mine_file.py -c [COLUMN_NAME] [filename] 
+python mine_file.py -c [COLUMN_NAME] -o [OUTPUT_FILE] [filename] 
+
 
 """
 
@@ -16,20 +17,23 @@ import json
 import csv
 
 # parses file to into json document specified by the column name
-def parse_file (filename, column):
+def parse_file (filename, column, out):
 	vacant_dict = {}
+	file_name,file_extension = os.path.splitext(filename)
+	file_type = ''
+	if ( file_extension == '.tsv' ): file_type = "\t"
+	elif (file_extension == '.csv'): file_type = ","
+
 	print("Loading biodata from {}".format(filename))
 	empty_dict = {}
 	with open(filename, 'rw') as tsvfile:
-		reader = csv.DictReader(tsvfile,delimiter=str("\t"), quoting=csv.QUOTE_NONE)
+		reader = csv.DictReader(tsvfile,delimiter=str(file_type), quoting=csv.QUOTE_NONE)
 		for row in reader:
 			type = row[column]  
 			vacant_dict[type] = empty_dict
 	data = json.dumps(vacant_dict)
-	out = 'data.json'
 	with open(out, 'w') as file:
 		file.write(data)
-	return out
 
 # main()
 def main():
@@ -40,12 +44,17 @@ def main():
 	parser.add_argument('filename', metavar='FILE_NAME',
                     help='specifies the name of file to be processed')
 	parser.add_argument('-c', metavar='COLUMN_NAME', type=str,
-					help='species the column to be processed into the json file')
+					help='specifies the column to be processed into the json file')
+	parser.add_argument('-o', metavar= 'OUTPUT_FILE', type=str, default='data.json',
+					help= 'specifies the name of the output file; it will be '
+						  'defaulted to data.json if none is specified')
+
 	args = parser.parse_args()
 	column = args.c
 	filename = args.filename
-	out = parse_file(filename, column)		
-	print ("Done! Output written to {}".format(out))
+	output = args.o
+	parse_file(filename, column, output)		
+	print ("Done! Output written to {}".format(output))
 
 # run main ----------------->
 if __name__ == "__main__":
